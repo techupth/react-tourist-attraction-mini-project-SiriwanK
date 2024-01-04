2b541ccde6057e152065e7a23be9aabb7b0f85fd
Nok <siriwan.skk@gmail.com>
2024-01-05 05:15:21 +0700

    feat: unfinished2

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,10 @@ import "../App.css";
 export function HomePage() {
   const [searchTrip, setSearchTrip] = useState("");
   const [content, setContent] = useState([]);
+  // const [tag, setTag] = useState([]);
+
+  // const maxLength = 100;
+
   const searchFromServer = async () => {
     try {
       const response = await axios.get(
@@ -32,6 +36,7 @@ export function HomePage() {
         onChange={(event) => setSearchTrip(event.target.value)}
         value={searchTrip}
       />
+      <hr width="80%" />
 
       <div className="content-list">
         {content.map((trip, index) => {
@@ -43,21 +48,46 @@ export function HomePage() {
                   alt={trip.title}
                   width="360"
                   height="240"
+                  className="wnd-stretch rounded-corners"
                 />
               </div>
               <div className="content-detail">
-                <h1 className="content-title">{trip.title}</h1>
+                <div className="content-title">{trip.title}</div>
                 <div className="content-description">
                   {trip.description}
-                  <a href={trip.url}>อ่านต่อ</a>
+                  <br />
+                  <a href={trip.url}> อ่านต่อ</a>
                 </div>
                 <div className="content-tag">
                   หมวด
-                  <div className="other-previews">
-                    <img src={trip.photos[1]} />
-                    <img src={trip.photos[2]} />
-                    <img src={trip.photos[3]} />
-                  </div>
+                  {/* {item.tags.map((tag, i) => {
+                    return (
+                      <div key={i} className="in-tag">
+                        {tag}
+                      </div>
+                    );
+                  })} */}
+                </div>
+
+                <div className="other-previews">
+                  <img
+                    src={trip.photos[1]}
+                    className="wnd-stretch rounded-corners2"
+                    width="120"
+                    height="120"
+                  />
+                  <img
+                    src={trip.photos[2]}
+                    className="wnd-stretch rounded-corners2"
+                    width="120"
+                    height="120"
+                  />
+                  <img
+                    src={trip.photos[3]}
+                    className="wnd-stretch rounded-corners2"
+                    width="120"
+                    height="120"
+                  />
                 </div>
               </div>
             </div>
